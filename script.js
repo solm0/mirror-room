3509c1e2e36f3cd34764e4d0ece3aa44541be208
정솔미 <148732006+solm0@users.noreply.github.com>
2023-11-28 09:34:27 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+navigator.mediaDevices.getUserMedia({
+    video: {
+        width: { ideal: 1 },
+        height: { ideal: 1 }
+    }
+})
+.then(function (stream) {
+    // Set stream for the first video element
+    document.getElementById('webcam1').srcObject = stream.clone();
+
+    // Set stream for the second video element
+    document.getElementById('webcam2').srcObject = stream.clone();
+
+    // Set stream for the third video element
+    document.getElementById('webcam3').srcObject = stream.clone();
+})
+
+.catch(function (error) {
+    console.error('Error accessing webcam:', error);
+});
+
